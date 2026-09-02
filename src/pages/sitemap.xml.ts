982d438db9d3b419725f775c91a318a70bd82aaf
YadiraAllauca <yadiraallaucac@gmail.com>
2026-09-01 22:44:11 -0500

    feat: add sitemap and robots.txt for SEO, update site metadata and improve layout structure

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ site }) => {
+	const home = new URL('/', site).href;
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+\t<url>
+\t\t<loc>${home}</loc>
+\t\t<changefreq>weekly</changefreq>
+\t\t<priority>1.0</priority>
+\t</url>
+</urlset>
+`;
+	return new Response(xml, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
+};
